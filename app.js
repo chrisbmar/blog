3861fc4b04aa4f18db92ed36b8d31a8348dd1ac1
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2019-09-04 18:36:27 +0200

    add get routes for main pages in the header

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home", {homeContent: homeContent});
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 
