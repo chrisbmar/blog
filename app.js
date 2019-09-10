9541cf6ffb78aabe9b8db1b5f84689c8a3c9d74f
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2019-09-10 22:24:37 +0200

    add POST request to mailchimps servers for newsletter

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const dotenv = require('dotenv').config();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const request = require('request');
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const listId = process.env.LIST_ID;
+const apiKey = process.env.API_KEY;
 
 const app = express();
 
@@ -31,7 +35,51 @@ app.get("/", (req, res) => {
       posts: posts.reverse(),
       });
   });
-});
+});   
+
+//post request to MAILCHIMP's servers
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+      members: [
+          {
+              email_address: email,
+              status: 'subscribed',
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName
+              }
+          }
+      ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const options = {
+      url: `https://us3.api.mailchimp.com/3.0/lists/${listId}`,
+      method: 'POST',
+      headers: {
+          'Authorization': `chris ${apiKey}`
+      },
+      body: jsonData
+  };
+
+  request(options, (error, response, body) => {
+      if (error) {
+          res.render("failure");
+      } else {
+          if (response.statusCode === 200) {
+            res.render("success");
+          } else {
+            res.render("failure");
+            console.log("3");
+          };
+      };
+  });
+}); 
 
 app.get("/compose", (req, res) => {
   res.render("compose");
@@ -77,7 +125,6 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-
 
 app.listen(4000, function() {
   console.log("Server started on port 4000");
